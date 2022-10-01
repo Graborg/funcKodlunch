06361f72a1a73f57aba893631814e299d1eda4e7
Mikael Gråborg <mikael.graborg@iteam.se>
2022-10-01 12:11:38 +0200

    higher order functions

--- kodlunch.js
+++ kodlunch.js
@@ -1,23 +1,54 @@
-const arr = [
+const sendMail = (data) => console.log("mailing", data);
+const saveToDb = (data) => console.log("saving", data);
+
+// function mailPreferences(arr) {
+//   let result = [];
+
+//   for (let index = 0; index < arr.length; index++) {
+//     const person = arr[index];
+//     if (person.age >= 18) {
+//       person.drink = "beer";
+//       result.push(person);
+//     } else {
+//     }
+//   }
+//   if (result) {
+//     sendMail(result);
+//   } else {
+//     console.log("nothing to send");
+//   }
+// }
+
+const persons = [
   {
-    name: 'Mikael',
+    name: "Mikael",
     age: 15,
+    meta: {
+      //lots of other info
+    },
   },
   {
-    name: 'Alexander',
+    name: "Alexander",
     age: 19,
+    meta: {
+      //lots of other info
+    },
   },
 ];
-let result = [];
 
-for (let index = 0; index < arr.length; index++) {
-  const person = arr[index];
-  if (person.age >= 18) {
-    person.drink = 'beer';
+function mailPreferencesFunc(arr) {
+  res = arr
+    .filter(({ age }) => age > 18)
+    .map((e) => {
+      e.drink = "beer";
+      return e;
+    });
+  if (res) {
+    sendMail(res);
   } else {
-    person.drink = 'water';
+    console.log("nothing to send");
   }
-  result.push(person);
 }
 
-console.log(result);
+mailPreferencesFunc(persons);
+saveToDb(persons);
