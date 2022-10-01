e8008502479d84eeb5047359adc6f756bbb4af92
Mikael Gråborg <mikael.graborg@iteam.se>
2022-10-01 12:50:08 +0200

    imperative with stats

--- kodlunch.js
+++ kodlunch.js
@@ -3,18 +3,25 @@ const saveToDb = (data) => console.log("saving", data);
 
 function mailPreferences(arr) {
   let result = [];
+  let beerDrinkers = 0;
+  let wineDrinkers = 0;
 
   for (let index = 0; index < arr.length; index++) {
     const person = arr[index];
     if (person.age >= 20) {
       person.drink = "wine";
       result.push(person);
+      wineDrinkers += 1;
     } else if (person.age >= 18) {
       person.drink = "beer";
       result.push(person);
+      beerDrinkers += 1;
     }
   }
   if (result) {
+    console.log(`wineDrinkers ${wineDrinkers / arr.length}%`);
+    console.log(`beerDrinkers ${beerDrinkers / arr.length}%`);
+    console.log(`excluded ${(arr.length - result.length) / arr.length}%`);
     sendMail(result);
   } else {
     console.log("nothing to send");
@@ -32,6 +39,13 @@ const persons = [
   {
     name: "Alexander",
     age: 19,
+    meta: {
+      //lots of other info
+    },
+  },
+  {
+    name: "Andreas",
+    age: 22,
     meta: {
       //lots of other info
     },
@@ -55,5 +69,5 @@ function mailPreferencesFunc(arr) {
   }
 }
 
-mailPreferencesFunc(persons);
+mailPreferences(persons);
 saveToDb(persons);
